3b971390c624fbdaa5bcf529a3a1903ab02a2f8e
Povilas Juzeliunas <pjuzeliunas@gmail.com>
2022-10-18 22:10:46 +0200

    Linker falgs added

--- Package.swift
+++ Package.swift
@@ -26,7 +26,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "CModbus"),
+        .target(
+            name: "CModbus",
+            linkerSettings: [.unsafeFlags(["-lbsd"])]
+        ),
         .target(
             name: "SwiftLibModbus",
             dependencies: ["CModbus"]),
